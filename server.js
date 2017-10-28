e7ddbbe38bf73e976938f05fb69d8a370472936f
Jaspreet Atwal <jatwal@capaxglobal.com>
2017-10-28 19:04:37 +0100

    Production config.

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require("express");
-const webpackMiddleware = require("webpack-dev-middleware");
-const webpack = require("webpack");
-const webpackConfig = require("./webpack.config");
+const path = require("path");
 
 const app = express();
 
-app.use(webpackMiddleware(webpack(webpackConfig)));
+app.get("/hello", (req, res) => res.send({ hi: "there" }));
 
-app.listen(3050, () => console.log("Listening"));
+if (process.env.NODE_ENV !== "production") {
+    const webpackMiddleware = require("webpack-dev-middleware");
+    const webpack = require("webpack");
+    const webpackConfig = require("./webpack.config");
+
+    app.use(webpackMiddleware(webpack(webpackConfig)));
+} else {
+    console.log(process.env.NODE_ENV);
+    app.use(express.static("dist"));
+    app.get("*", () => (req, res) => {
+        res.sendFile(path.join(__dirname, "dist/index.html"));
+    });
+}
+
+app.listen(process.env.PORT || 3050, () => console.log("Listening"));

e53796ed4711f60a571e7bf5a5da7913e4b01620
Jaspreet Atwal <jatwal@capaxglobal.com>
2017-10-28 18:34:05 +0100

    Reverted changes.

--- src/router.js
+++ src/router.js
@@ -6,39 +6,50 @@ import Home from './components/Home';
 // most visited route and therefore we don't want to do
 // another round trip to the server to fetch this component
 import ArtistMain from './components/artists/ArtistMain';
+import ArtistCreate from './components/artists/ArtistCreate';
+import ArtistDetail from './components/artists/ArtistDetail';
+import ArtistEdit from './components/artists/ArtistEdit';
 
-const componentRoutes = {
-  component: Home,
-  path: "/",
-  IndexRoute: { component: ArtistMain },
-  childRoutes: [
-    {
-      path: "artist/new",
-      getComponent(location, cb) {
-        System.import("./components/artists/ArtistCreate")
-          .then(module => cb(null, module.default));
-      }
-    },
-    {
-      path: "artist/:id",
-      getComponent(location, cb) {
-        System.import("./components/artists/ArtistDetail")
-          .then(module => cb(null, module.default));
-      }
-    },
-    {
-      path: "artist/:id/edit",
-      getComponent(location, cb) {
-        System.import("./components/artists/ArtistEdit")
-          .then(module => cb(null, module.default));
-      }
-    }
-  ]
-};
+// const componentRoutes = {
+//   component: Home,
+//   path: "/",
+//   IndexRoute: { component: ArtistMain },
+//   childRoutes: [
+//     {
+//       path: "artist/new",
+//       getComponent(location, cb) {
+//         System.import("./components/artists/ArtistCreate")
+//           .then(module => cb(null, module.default));
+//       }
+//     },
+//     {
+//       path: "artist/:id",
+//       getComponent(location, cb) {
+//         System.import("./components/artists/ArtistDetail")
+//           .then(module => cb(null, module.default));
+//       }
+//     },
+//     {
+//       path: "artist/:id/edit",
+//       getComponent(location, cb) {
+//         System.import("./components/artists/ArtistEdit")
+//           .then(module => cb(null, module.default));
+//       }
+//     }
+//   ]
+// };
 
 const Routes = () => {
   return (
-     <Router history={hashHistory} routes={componentRoutes} />
+    // <Router history={hashHistory} routes={componentRoutes} />
+    <Router history={hashHistory}>
+    <Route path="/" component={Home}>
+      <IndexRoute component={ArtistMain} />
+      <Route path="artists/new" component={ArtistCreate} />
+      <Route path="artists/:id" component={ArtistDetail} />
+      <Route path="artists/:id/edit" component={ArtistEdit} />
+    </Route>
+  </Router>
   );
 };
 
